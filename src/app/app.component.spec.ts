bc06833a71735960cca1e6ee049a86386d5d5e50
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-09-25 06:34:29 -0400

    building up tests and figuring out what is what and breaking stuff but finding out why and fixing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('Base Site', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -19,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as site title 'Joe's Angular Site'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual("Joe's Angular Site");
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render heading in a h4 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h4').textContent).toContain("Welcome to Joe's Super Awesome Angular Site.");
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+// This is done in Typescript angular 2 is done with it
+//Everything is also done with ES6/ES7
+
 import { Component } from '@angular/core';
+//@things are decorators and are:
+//an expression
+//that evaluates to a function
+//that takes the target, name, and property descriptor as arguments
+//and optionally returns a property descriptor to install on the target object
 
 @Component({
-  selector: 'my-app-root',
+  selector: 'site-header',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  sitewelcome = "Welcome to Joe's Super Awesome Angular Site.";
+  // mainImage = 'https://avatars1.githubusercontent.com/u/13924793?v=4&s=220';
+  title = "Joe's Angular Site";
 }
